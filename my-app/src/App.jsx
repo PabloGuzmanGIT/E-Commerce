bca5090319d290c115b50f76154f953a92475516
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-02-19 00:12:17 -0500

    cambios en apps joseph

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,17 +4,11 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-<<<<<<< HEAD
-
-import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-=======
 import { Provider } from "react-redux";
->>>>>>> bd564c1c0a76959924860d548a9838bb68c7792e
 import { Header } from "./components/header/header";
 import { Main } from "./components/main/main";
 import { Footer } from "./components/footer/footer";
-
 import { PageAbout } from "./pages/about/about";
 import { PageHome } from "./pages/home/home";
 import { PageCatalog } from "./pages/catalog/catalog";
@@ -24,10 +18,8 @@ import { PageContact } from "./pages/contact/contact";
 import { PageLogin } from "./pages/login/login";
 import { PageRegister } from "./pages/register/register";
 import { PageNotFound } from "./pages/not-found/not-found";
-import { store } from "./store/store";
 import "antd/dist/antd.css";
 import "./assets/style/main.scss";
-
 import { store, persistor } from "./store/store";
 
 /*
@@ -36,7 +28,6 @@ Componente wrapper
 export function App() {
   return (
     <Provider store={store}>
-<<<<<<< HEAD
     <PersistGate loading={null} persistor={persistor}>
     <Router>
       <div className="wrapper flex-col items-center flex">
@@ -64,6 +55,9 @@ export function App() {
             <Route path="/login">
               <PageLogin />
             </Route>
+            <Route path="/register">
+                <PageRegister />
+              </Route>
             <Redirect exact from="/" to="/home" />
             <Route path="*">
               <PageNotFound />
@@ -73,44 +67,7 @@ export function App() {
         <Footer />
       </div>
     </Router>
-    </PersistGate>
-=======
-      <Router>
-        <div className="wrapper flex-col items-center flex">
-          <Header />
-          <Main>
-            <Switch>
-              <Route path="/home">
-                <PageHome />
-              </Route>
-              <Route path="/catalog">
-                <PageCatalog />
-              </Route>
-              <Route path="/delivery">
-                <PageDelivery />
-              </Route>
-              <Route path="/contact">
-                <PageContact />
-              </Route>
-              <Route path="/about">
-                <PageAbout />
-              </Route>
-              <Route path="/login">
-                <PageLogin />
-              </Route>
-              <Route path="/register">
-                <PageRegister />
-              </Route>
-              <Redirect exact from="/" to="/home" />
-              <Route path="*">
-                <PageNotFound />
-              </Route>
-            </Switch>
-          </Main>
-          <Footer />
-        </div>
-      </Router>
->>>>>>> bd564c1c0a76959924860d548a9838bb68c7792e
+    </PersistGate>      
     </Provider>
   );
 }
